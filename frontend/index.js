057d6f80ceccafa04b483dcf398b3892cdbcffc3
Florent Le Gall <florent.legall@gmail.com>
2019-06-27 22:57:56 +0200

    Add frontend react demo app

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
